be70df0cff4d967a868c4a9534679fc361efaed5
Ronan D'Souza <ronan696@gmail.com>
2018-01-31 22:15:34 +0530

    Update z_gdrive_viewer_static.php

--- z_gdrive_viewer_static.php
+++ z_gdrive_viewer_static.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="/css/style.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
 <?php
 	include_once 'simple_html_dom.php';
 	function zfunc($ID)
